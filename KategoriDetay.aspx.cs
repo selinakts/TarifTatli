477d6487d334707a6850afac58c68228758a02a5
selinakts <selinakts98@gmail.com>
2021-05-11 08:55:34 +0300

    update categories

---
+++ KategoriDetay.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class KategoriDetay : System.Web.UI.Page
+{
+    data baglan = new data();
+    string kategoriid = "";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        kategoriid = Request.QueryString["kategoriid"];
+        SqlCommand komut = new SqlCommand("Select * From Table_Tatliler Where kategoriid=@p1", baglan.bag());
+        komut.Parameters.AddWithValue("@p1", kategoriid);
+        SqlDataReader dr = komut.ExecuteReader();
+        DataList2.DataSource = dr;
+        DataList2.DataBind();
+    }
+}
